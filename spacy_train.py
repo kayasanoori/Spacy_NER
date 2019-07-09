5eaad42138574866e509d09f28f6bca0b0957652
shank <shanksprojects@gmail.com>
2019-07-09 18:38:41 +0530

    Add files via upload

---
+++ spacy_train.py
@@ -0,0 +1,133 @@
+
+from __future__ import unicode_literals, print_function
+
+import plac
+import random
+from pathlib import Path
+import spacy
+#from spacy.gold import GoldParse
+#from spacy.language import EntityRecognizer
+#from spacy.util import minibatch, compounding
+
+############################################  NOTE  ########################################################
+#
+#           Creates NER training data in Spacy format from JSON downloaded from Dataturks.
+#
+#           Outputs the Spacy training data which can be used for Spacy training.
+#
+############################################################################################################
+import json
+def convert_dataturks_to_spacy(dataturks_JSON_FilePath):
+    try:
+        training_data = []
+        lines=[]
+        with open(dataturks_JSON_FilePath, 'r') as f:
+            lines = f.readlines()
+            
+
+        for line in lines:
+            data = json.loads(line)
+            
+            text = data['content']
+            entities = []
+            for annotation in data['annotation']:
+                #only a single point in text annotation.
+                point = annotation['points'][0]
+                labels = annotation['label']
+                # handle both list of labels or a single label.
+                if not isinstance(labels, list):
+                    labels = [labels]
+
+                for label in labels:
+                    #dataturks indices are both inclusive [start, end] but spacy is not [start, end)
+                    entities.append((point['start'], point['end'] + 1 ,label))
+
+
+            training_data.append((text, {"entities" : entities}))
+
+        return training_data
+    except Exception as e:
+        return e
+    
+
+
+@plac.annotations(
+    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    output_dir=("Optional output directory", "option", "o", Path),
+    n_iter=("Number of training iterations", "option", "n", int),
+)
+def main(model=None, output_dir='', n_iter=100):
+    #If you want to train existing change None to model path 
+    #Path of your annotated file 
+    
+    TRAIN_DATA = convert_dataturks_to_spacy(""); 
+    """Load the model, set up the pipeline and train the entity recognizer."""
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank("en")  # create blank Language class
+        print("Created blank 'en' model")
+
+    # create the built-in pipeline components and add them to the pipeline
+    # nlp.create_pipe works for built-ins that are registered with spaCy
+    if "ner" not in nlp.pipe_names:
+        ner = nlp.create_pipe("ner")
+        nlp.add_pipe(ner, last=True)
+    # otherwise, get it so we can add labels
+    else:
+        ner = nlp.get_pipe("ner")
+
+    # add labels
+    for _, annotations in TRAIN_DATA:
+        for ent in annotations.get("entities"):
+            ner.add_label(ent[2])
+
+    # get names of other pipes to disable them during training
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
+    with nlp.disable_pipes(*other_pipes):  # only train NER
+        # reset and initialize the weights randomly – but only if we're
+        # training a new model
+        if model is None:
+            nlp.begin_training()
+            
+        for itn in range(n_iter):
+            random.shuffle(TRAIN_DATA)
+            losses = {}
+            for text, annotations in TRAIN_DATA:
+                nlp.update(
+                    [text],  # batch of texts
+                    [annotations],  # batch of annotations
+                    drop=0.35,  # dropout - make it harder to memorise data
+                    losses=losses)
+            
+            print("Losses", losses)
+
+    # test the trained model
+    for text, _ in TRAIN_DATA:
+        doc = nlp(text)
+        print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+        print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+
+    # save model to output directory
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
+
+        # test the saved model
+        print("Loading from", output_dir)
+        nlp2 = spacy.load(output_dir)
+        for text, _ in TRAIN_DATA:
+            doc = nlp2(text)
+            print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+            print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+
+
+    
+    
+
+if __name__ == "__main__":
+    plac.call(main)
